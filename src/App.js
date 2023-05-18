3b476e5b367a3ad8c0b31e3cbf52055e47dea042
Marcelle Vargas <marcelle.vargas96@gmail.com>
2023-05-18 09:00:07 -0300

    fix: web load

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import { Application } from '@splinetool/runtime';
 
 const canvas = document.getElementById('canvas3d');
 const app = new Application(canvas);
+const spline = app.load('https://prod.spline.design/ARZRwPxk23aAQlzZ/scene.splinecode');
 
-app.load('https://prod.spline.design/ARZRwPxk23aAQlzZ/scene.splinecode');
+function App() {
+  return (
+    <div className="spline">
+     {spline}
+    </div>
+  );
+}
 
+export default App;
